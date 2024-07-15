1435d7ba5bf7c1015684f3e022c0c77cfa4c453b
Rajan <rajanmasih154@gmail.com>
2024-07-16 01:37:08 +0530

    Assignment 1

---
+++ assignment-1/assignment_1.c
@@ -0,0 +1,129 @@
+// Q-1
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     printf("Hello\n");
+//     return 0;
+// }
+
+// Q-2
+
+// #include<stdio.h>
+
+// int main() {
+//     printf("Hello\n");
+//     printf("Students\n");
+//     return 0;
+// }
+
+// Q-3
+
+//  \" ---> "
+//  \"\" ---> ""
+
+// #include<stdio.h>
+
+// int main() {
+//     // puts("\"Hello\"");
+//     printf("\"MySirG");
+//     printf("\n");
+//     return 0;
+// }
+
+// Q-4
+
+// #include<stdio.h>
+
+// int main() {
+//     printf("Hello\nDuniya\n");
+//     return 0;
+// }
+
+// Q-5
+
+// \r ---> agr maine kuch likha hai like Hello then \r dal diya tb voh first se replace krna shuru
+// kr dega jisko Carriage Return kehte hai
+
+// #include<stdio.h>
+
+// int main() {
+//     printf("Hello \rNavdeep");
+//     printf("\n");
+//     return 0;
+// }
+
+// Q-6
+
+// #include<stdio.h>
+// int main() {
+//     printf("\"Teacher's Day\"\n");
+//     return 0;
+// }
+
+// Q-7
+// #include<stdio.h>
+
+// int main() {
+//     int firstNumber, secondNumber, result;
+//     printf("Enter first number: ");
+//     scanf("%d", &firstNumber);
+//     printf("\n");
+//     printf("Enter second number: ");
+//     scanf("%d", &secondNumber);
+//     printf("\n");
+//     result = firstNumber + secondNumber;
+//     printf("Result = %d" ,result);
+//     printf("\n");
+//     return 0;
+// }
+
+// Q-8
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int squareNumber;
+//     printf("Enter number to do square: ");
+//     scanf("%d", &squareNumber);
+//     printf("\n");
+//     squareNumber = squareNumber * squareNumber;
+//     printf("Result = %d", squareNumber);
+//     printf("\n");
+//     return 0;
+// }
+
+// Q-9
+// #include<stdio.h>
+// int main() {
+//     // A = L * B;
+//     int areaOfRectangle, Length, Breadth;
+//     printf("Enter the length: ");
+//     scanf("%d", &Length);
+//     printf("\n");
+//     printf("Enter the breadth: ");
+//     scanf("%d", &Breadth);
+//     printf("\n");
+//     areaOfRectangle = Length * Breadth;
+//     printf("Result = %d", areaOfRectangle);
+//     printf("\n");
+//     return 0;
+// }
+
+// Q-10
+
+// #include<stdio.h>
+// int main() {
+//     // A = r^2;
+//     int area, number;
+//     printf("Enter the number: ");
+//     scanf("%d", &number);
+//     printf("\n");
+//     number = number * number;
+//     area = 3.14 * number;
+//     printf("Result = %d", area);
+//     printf("\n");
+//     return 0;
+// }
