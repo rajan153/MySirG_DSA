ec975f200cf8dd3ca7ff4123328db3ee28af4833
Rajan <rajanmasih154@gmail.com>
2024-07-17 23:14:37 +0530

    Assignment 2

---
+++ assignment-2/assignment-2.c
@@ -0,0 +1,90 @@
+// Q-1
+
+// #include<stdio.h>
+
+// int main() {
+//     int first, second, third, result;
+//     printf("Enter the first number: ");
+//     scanf("%d", &first);
+//     printf("\n");
+//     printf("Enter the second number: ");
+//     scanf("%d", &second);
+//     printf("\n");
+//     printf("Enter the third number: ");
+//     scanf("%d", &third);
+//     printf("\n");
+//     result = (first + second + third)/3;
+//     printf("Result = %d", result);
+//     printf("\n");
+//     return 0;
+// }
+
+// Q-2
+
+// #include<stdio.h>
+
+// int main() {
+//     int radius;
+//     double pi = 3.14, result;
+//     printf("Enter the radius of circle: ");
+//     scanf("%d", &radius);
+//     printf("\n");
+//     result = 2 * pi * radius;
+//     printf("Result = %f", result);
+//     printf("\n");
+//     return 0;
+// }
+
+// Q-3
+
+// #include<stdio.h>
+
+// int main() {
+//     int simpleInterst, principalAmount, timePeriod;
+//     float rateOfInterest;
+//     printf("Enter the Principal Amount: ");
+//     scanf("%d", &principalAmount);
+//     printf("\n");
+//     printf("Enter the Rate of Interest: ");
+//     scanf("%f", &rateOfInterest);
+//     printf("\n");
+//     printf("Enter the Time Period: ");
+//     scanf("%d", &timePeriod);
+//     printf("\n");
+//     simpleInterst = principalAmount * rateOfInterest * timePeriod;
+//     printf("Result = %d", simpleInterst);
+//     printf("\n");
+//     return 0;
+// }
+
+// Q-4
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     float length, width, height, result;
+//     printf("Enter the length: ");
+//     scanf("%f", &length);
+//     printf("\n");
+//     printf("Enter the width: ");
+//     scanf("%f", &width);
+//     printf("\n");
+//     printf("Enter the height: ");
+//     scanf("%f", &height);
+//     printf("\n");
+//     result = length * width * height;
+//     printf("Result = %f", result);
+//     printf("\n");
+
+//     return 0;
+// }
+
+// Q-5
+
+#include<stdio.h>
+
+int main() {
+    
+    return 0;
+}
